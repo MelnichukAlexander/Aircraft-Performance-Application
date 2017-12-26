785be061de0b84f01ea5523a984d14627fa3060b
Александр <aleksandr@MacBook-Air-Aleksandr.local>
2017-12-26 23:26:08 +0300

    Initial Commit

---
+++ Aircraft Performance Application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aircraft Performance Application
+//
+//  Created by Александр on 26.12.17.
+//  Copyright © 2017 Alexander Melnichuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
